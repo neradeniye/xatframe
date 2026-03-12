1cc21f5166e3c161635d17a30305b6a4da7e547e
neradeniye <neradeniye@gmail.com>
2026-03-12 17:13:23 -0500

    update

--- js/themes.js
+++ js/themes.js
@@ -1,9 +1,13 @@
 var link = [];
-link[0] = "./css/testing.css";
-/*
+//link[0] = "./css/testing.css";
+
 link[0] = "/xatframe/css/beato.css";
 link[1] = "/xatframe/css/bern.css";
-*/
+link[2] = "/xatframe/css/shannon.css";
+link[3] = "/xatframe/css/beato_battle.css";
+link[4] = "/xatframe/css/ange_sky.css";
+link[5] = "/xatframe/css/eva_gun.css";
+link[6] = "/xatframe/css/ange_maria.css";
 
 $(function() {
     var style = link[Math.floor(Math.random() * link.length )];
