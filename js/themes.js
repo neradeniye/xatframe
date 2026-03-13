288f1650fe84729d6fdf1d8e895c8db8211862fb
neradeniye <neradeniye@gmail.com>
2026-03-12 20:54:02 -0500

    update

--- js/themes.js
+++ js/themes.js
@@ -1,22 +1,56 @@
-var link = [];
-//link[0] = "./css/testing.css";
-
-link[0] = "/xatframe/css/beato.css";
-link[1] = "/xatframe/css/bern.css";
-link[2] = "/xatframe/css/shannon.css";
-link[3] = "/xatframe/css/beato_battle.css";
-link[4] = "/xatframe/css/ange_sky.css";
-link[5] = "/xatframe/css/eva_gun.css";
-link[6] = "/xatframe/css/ange_maria.css";
+var categories = {
+    umineko: {
+        folder: "/xatframe/css/umineko/",
+		//folder: "./css/umineko/",
+        files: [
+			//"testing.css",
+            "true_beato.css",
+            "bern.css",
+            "shannon.css",
+            "beato_battle.css",
+            "ange_sky.css",
+            "eva_gun.css",
+            "clair_will.css",
+            "beato_battler.css",
+            "beato_battler2.css",
+            "bern_lambda.css",
+            "erika_beato.css",
+            "lion_dead.css",
+            "lion_will.css"
+			"evil_battler.css"
+			"rosa_maria.css"
+        ]
+    },
+    higurashi: {
+        folder: "/xatframe/css/higurashi/",
+        files: [
+            "rena.css",
+			"rena2.css",
+			"rena_mion.css",
+			"rika.css",
+			"keiichi_rena.css"
+        ]
+    }
+    // Add more categories here as needed, e.g.,
+    // anotherCategory: {
+    //     folder: "/xatframe/css/anotherCategory/",
+    //     files: [...]
+    // }
+};
 
 $(function() {
-    var style = link[Math.floor(Math.random() * link.length )];
-    if (document.createStyleSheet){
+    var catNames = Object.keys(categories);
+    var selectedCat = catNames[Math.floor(Math.random() * catNames.length)];
+    var cat = categories[selectedCat];
+    var file = cat.files[Math.floor(Math.random() * cat.files.length)];
+    var style = cat.folder + file;
+    
+    if (document.createStyleSheet) {
         document.createStyleSheet(style);
-    }else{
-        $('<link />',{
-            rel :'stylesheet',
-            type:'text/css',
+    } else {
+        $('<link />', {
+            rel: 'stylesheet',
+            type: 'text/css',
             href: style
         }).appendTo('head');
     }
